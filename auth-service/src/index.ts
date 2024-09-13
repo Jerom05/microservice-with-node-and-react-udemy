7a9d3ff7658022116a06b2faaea912ae3535f56b
jerom-ghagra <jeromghagra@gmail.com>
2024-09-14 00:45:25 +0600

    feat: Add initial auth service routes

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from 'express'
 
+import {
+  currentUserRouter,
+  signInRouter,
+  signOutRouter,
+  signUpRouter
+} from './routes'
+
 const app = express()
-const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Auth-service')
 })
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
 
+const port = 3000
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
 })
---
+++ auth-service/src/routes/index.ts
@@ -0,0 +1,4 @@
+export { router as currentUserRouter } from './current-user'
+export { router as signInRouter } from './signin'
+export { router as signOutRouter } from './signout'
+export { router as signUpRouter } from './signup'
