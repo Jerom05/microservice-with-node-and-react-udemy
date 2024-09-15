fe466880893948e0e3b2cbed2f0ec7c2b8a487cb
jerom-ghagra <jeromghagra@gmail.com>
2024-09-16 02:08:02 +0600

    feat: add signup feature

--- auth-service/src/routes/signup.ts
+++ auth-service/src/routes/signup.ts
@@ -1,9 +1,8 @@
 import express from 'express'
+import { signup } from '../controller/user-controller'
 
 const router = express.Router()
 
-router.get('/api/users/signup', (req, res) => {
-  res.send('signup')
-})
+router.get('/api/users/signup', signup)
 
 export { router }
