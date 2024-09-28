2ff7ff8d40e6f9c41601296d2f1f6ce9af7945e0
jerom-ghagra <jeromghagra@gmail.com>
2024-09-28 10:05:17 +0600

    feat: Add input validation and error handling for signup route

--- auth-service/src/routes/signup.ts
+++ auth-service/src/routes/signup.ts
@@ -1,8 +1,19 @@
 import express from 'express'
+import { body } from 'express-validator'
+
+import { validateRequest } from '../middlewares/validate-request'
 import { signup } from '../controller/user-controller'
 
 const router = express.Router()
 
-router.get('/api/users/signup', signup)
+const validations = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters')
+]
+
+router.post('/api/users/signup', validations, validateRequest, signup)
 
 export { router }
