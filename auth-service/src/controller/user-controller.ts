5ef12d064ca84b388391f6ac38a4ea976cfc32f7
jerom ghagra <jeromghagra@gmail.com>
2024-10-16 09:23:04 +0600

    feat: add signin route

--- auth-service/src/controller/user-controller.ts
+++ auth-service/src/controller/user-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
 import { User } from '../models/user'
 
 export const signup = async (req: Request, res: Response) => {
@@ -22,4 +24,28 @@ export const signup = async (req: Request, res: Response) => {
   req.session = { jwt: token }
 
   res.status(200).send({ user })
+}
+
+export const signin = async (req: Request, res: Response) => {
+  const { email, password } = req.body || {}
+
+  const existingUser = await User.findOne({ email })
+  if (!existingUser) {
+    throw new BadRequestError('Invalid credentials')
+  }
+
+  const passwordMatch = await Password.compare(existingUser.password, password)
+
+  if (!passwordMatch) {
+    throw new BadRequestError('Invalid credentials')
+  }
+
+  const token = jwt.sign(
+    { id: existingUser.id, email: existingUser.email },
+    process.env.JWT_KEY!
+  )
+
+  req.session = { jwt: token }
+
+  res.status(200).send({ user: existingUser })
 }
--- auth-service/src/routes/signin.ts
+++ auth-service/src/routes/signin.ts
@@ -1,9 +1,16 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+
+import { validateRequest } from '../middlewares/validate-request'
+import { signin } from '../controller/user-controller'
 
 const router = express.Router()
 
-router.get('/api/users/signin', (req, res) => {
-  res.send('signin route')
-})
+const validations = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('You must supply a password')
+]
+
+router.post('/api/users/signin', validations, validateRequest, signin)
 
 export { router }
