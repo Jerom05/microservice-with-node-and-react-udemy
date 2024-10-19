b9da2810c368f82ace548563b6ce556ec85123d7
jerom ghagra <jeromghagra@gmail.com>
2024-10-19 18:36:39 +0600

    refactor: add app.ts

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,44 +1,6 @@
-import { json } from 'body-parser'
-import cookieSession from 'cookie-session'
-import dotenv from 'dotenv'
-import express, { Request, Response } from 'express'
-import 'express-async-errors'
 import mongoose from 'mongoose'
 
-import {
-  currentUserRouter,
-  signInRouter,
-  signOutRouter,
-  signUpRouter
-} from './routes'
-import { errorHandler } from './middlewares'
-import { NotFoundError } from './errors'
-
-dotenv.config()
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-)
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Auth-service')
-})
-
-app.use(currentUserRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(signUpRouter)
-
-app.all('*', async (req: Request, res: Response) => {
-  throw new NotFoundError()
-})
-app.use(errorHandler)
+import { app } from './app'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
