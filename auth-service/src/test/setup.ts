9c7600659063de574f0dc25b44654fb822ead76d
jerom ghagra <jeromghagra@gmail.com>
2024-10-20 10:07:22 +0600

    add global signin

--- auth-service/src/test/setup.ts
+++ auth-service/src/test/setup.ts
@@ -1,6 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
+import request from 'supertest'
 import mongoose from 'mongoose'
 import { afterAll, beforeAll, beforeEach } from '@jest/globals'
+
+import { app } from '../app'
+
+declare global {
+  function signin(): Promise<string[]>
+}
 
 let mongoServer: any
 
@@ -26,3 +33,18 @@ afterAll(async () => {
   await mongoServer.stop()
   await mongoose.connection.close()
 })
+
+global.signin = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(200)
+
+  const cookie = response.get('Set-Cookie')
+  if (!cookie) throw new Error('Cookie not set')
+
+  return cookie
+}
