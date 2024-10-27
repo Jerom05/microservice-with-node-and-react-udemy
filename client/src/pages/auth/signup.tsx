96584507824cfcbf1ff5eef7243c4ec39bbba4e7
jerom ghagra <jeromghagra@gmail.com>
2024-10-28 01:30:44 +0600

    feat(client): add signup page component

---
+++ client/src/pages/auth/signup.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import useRequest from '../../app/hooks/use-request'
+import '../../app/globals.css'
+
+const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: { email, password },
+  })
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await doRequest()
+  }
+
+  return (
+    <div className="container m-5">
+      <h1 className="text-2xl font-bold mb-5">Sign Up</h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group mb-5">
+          <p className="text-sm mb-1">Email Address</p>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border border-gray-300 rounded-md p-2"
+          />
+        </div>
+        <div className="form-group mb-5">
+          <p className="text-sm mb-1">Password</p>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border border-gray-300 rounded-md p-2"
+          />
+        </div>
+        {errors}
+        <div className="form-group mb-5">
+          <button className="bg-blue-500 text-white rounded-md p-2">
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
