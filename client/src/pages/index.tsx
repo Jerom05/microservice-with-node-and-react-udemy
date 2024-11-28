e1c1c8a568f213a60787cf6129dd8a0af74c892d
jerom ghagra <jeromghagra@gmail.com>
2024-11-28 20:02:45 +0600

    feat: get current user data

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import buildClient from '@/app/api/build-client'
+import { NextApiRequest } from 'next'
+import { GetServerSideProps } from 'next'
+
+interface HomeProps {
+  currentUser: object | null
+}
+
+const Home = ({ currentUser }: HomeProps) => {
+  console.log({ currentUser })
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home page!</h1>
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const client = buildClient({ req: context.req as NextApiRequest })
+    const response = await client.get('/api/users/currentuser')
+
+    return { props: { currentUser: response.data } }
+  } catch (error) {
+    console.log({ error })
+    return { props: {} }
+  }
+}
+
+export default Home
