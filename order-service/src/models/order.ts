bdf28821f71b3b1781ef38debc2532016d1a2a16
jerom ghagra <jeromghagra@gmail.com>
2025-01-24 07:48:38 +0600

    feat: implement ticket reservation check in order creation route and remove unused new order file

--- order-service/src/models/order.ts
+++ order-service/src/models/order.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
 import { OrderStatus } from '@caltickets/common'
 import { TicketDoc } from './ticket'
+
+export { OrderStatus }
+
 interface OrderAttrs {
   userId: string
   status: OrderStatus
