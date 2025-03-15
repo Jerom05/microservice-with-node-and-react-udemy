b50279cbd21c11f82aadf7ed0a9aab8d474ef5d7
jerom ghagra <jeromghagra@gmail.com>
2025-03-15 10:02:59 +0600

    feat: add OrderCreatedListener and OrderCancelledListener to the application startup

--- ticket-service/src/index.ts
+++ ticket-service/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 
 import { natsWrapper } from './nats-wrapper'
 import { app } from './app'
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -33,6 +35,9 @@ const start = async () => {
     })
     process.on('SIGINT', () => natsWrapper.client.close())
     process.on('SIGTERM', () => natsWrapper.client.close())
+
+    new OrderCreatedListener(natsWrapper.client).listen()
+    new OrderCancelledListener(natsWrapper.client).listen()
   } catch (err) {
     console.error(err)
   }
